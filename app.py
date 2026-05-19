c9e7e324b31f5020b026364bc15ebac5c916ec55
sirbakbaks-png <sirbak.baks@gmail.com>
2026-05-19 21:13:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -125,9 +125,9 @@ if "output" in st.session_state:
     # =========================
     # 📋 BOUTON COPIE (NATIVE STREAMLIT)
     # =========================
-    #st.code(st.session_state.output)
+   st.markdown("### 📋 Copier le résultat")
 
-    #st.info("📋 Clique dans le texte puis fais Ctrl+C (ou appui long sur téléphone)")
+    st.info("👉 Clique dans le texte puis Ctrl+C (ou appui long sur téléphone)")
     
     # =========================
     # 📄 AFFICHAGE (EDITABLE)
